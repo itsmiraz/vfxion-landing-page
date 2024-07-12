569b52bd5330271ce0b7f37c4f06f6f08279a310
itsmiraz <mirajulislamofficial044@gmail.com>
2024-07-12 09:23:53 +0600

    new section done

---
+++ src/constants/index.jsx
@@ -0,0 +1,27 @@
+import InnovateIcon from '../assets/icons/idea.png';
+import ViralIcon from '../assets/icons/people.png';
+import DollarIcon from '../assets/icons/dollar.png';
+import ImageIcon from '../assets/icons/image.png';
+
+export const aboutUs_content = [
+    {
+        icon: InnovateIcon,
+        title: "Innovation",
+        desc: "We continuously explore new software and technologies to innovate and meet our clients' goals. Our commitment to industry advancements ensures we deliver cutting-edge solutions tailored to each project's requirements."
+    },
+    {
+        icon: ViralIcon,
+        title: "Viral",
+        desc: "Achieving viral success is our top priority. With over 70 million views on TikTok, YouTube, and Instagram in 7 years, we've achieved this impressive reach entirely through organic growth, without any ad spend."
+    },
+    {
+        icon: DollarIcon,
+        title: "Results",
+        desc: "As professionals, we prioritize results. In 7 years, our viral marketing has generated over CAD $43 million for clients across finance, fashion, food, car retail, events, video games and much more."
+    },
+    {
+        icon: ImageIcon,
+        title: "Results",
+        desc: "No project is complete until the client is satisfied. We are dedicated to continuous innovation and exceeding quality standards. Our clients are consistently satisfied before they even need to mention it."
+    }
+]
